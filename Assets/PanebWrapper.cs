c57faf81f1c60a63c87c62d3793861f83e5b5637
Valentin Fries <contact@fries.io>
2018-03-05 15:57:20 +0100

    paneb-unity setup

---
+++ Assets/PanebWrapper.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.InteropServices;
+
+public static class PanebWrapper {
+
+	[DllImport("paneb")]
+	public static extern double super_add(double value);
+}
---
+++ Assets/TestPanebScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPanebScript : MonoBehaviour {
+
+	void Start () {
+		Debug.Log(PanebWrapper.super_add(7.0));
+	}
+}
