3d9a7768214fe3b4c93412a32ddc0323b019e831
CaptpBdcht <vincent_mot@hotmail.fr>
2018-03-05 18:53:02 +0100

    Added first exercice scene

---
+++ Assets/LinearlySeparableThreeScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearlySeparableThreeScript : MonoBehaviour {
+
+	public Transform[] spheres;
+
+	// Use this for initialization
+	void Start () {
+		foreach(var sphere in spheres) {
+			Debug.Log(sphere.position.x);
+			Debug.Log(sphere.position.z);
+		}	
+	}
+}
