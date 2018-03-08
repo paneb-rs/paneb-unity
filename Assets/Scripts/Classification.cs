ea899d4659e12fd70e920528375aaa5f97c53177
Valentin Fries <contact@fries.io>
2018-03-08 11:14:17 +0100

    Refactored Scenes directory

---
+++ Assets/Scripts/Classification.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Classification : MonoBehaviour {
+
+    public GameObject[] spheres;
+    public Transform[] whites;
+
+    private void PrintWeights(System.IntPtr weights)
+    {
+        Debug.Log(
+            PanebWrapper.classification_weights(weights, 0) + "; " +
+            PanebWrapper.classification_weights(weights, 1) + "; " +
+            PanebWrapper.classification_weights(weights, 2)
+        );
+    }
+
+    private void TrainModel(System.IntPtr weights)
+    {
+        for (int i = 0; i < 9001; ++i)
+        {
+            foreach (var sphere in spheres)
+            {
+                var transform = sphere.GetComponent<Transform>();
+
+                var x = transform.position.x;
+                var z = transform.position.z;
+                var expected = (int)transform.position.y;
+
+                PanebWrapper.classification_train(weights, x, z, expected);
+            }
+        }
+    }
+
+    private void MoveAxis(System.IntPtr weights)
+    {
+        foreach (var white in whites)
+        {
+            var x = white.position.x;
+            var z = white.position.z;
+
+            int direction = PanebWrapper.classification_compute(weights, x, z);
+            white.position += direction == 1 ? Vector3.up : Vector3.down;
+        }
+    }
+
+    void Start()
+    {
+		var weights = PanebWrapper.classification_create ();
+		TrainModel (weights);
+		MoveAxis (weights);
+    }
+}
---
+++ Assets/Scripts/Regression.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Regression : MonoBehaviour {
+
+    public GameObject[] spheres;
+    public Transform[] whites;
+
+    private double[] ComputeInputs()
+    {
+        List<double> inputs = new List<double>();
+
+        foreach (var sphere in spheres)
+        {
+            var transform = sphere.GetComponent<Transform>();
+
+            var x = transform.position.x;
+            var z = transform.position.z;
+
+            inputs.Add(1.0);
+            inputs.Add(x);
+            inputs.Add(z);
+        }
+
+        return inputs.ToArray();
+    }
+
+    private double[] ComputeOutputs()
+    {
+        List<double> outputs = new List<double>();
+
+        foreach (var sphere in spheres)
+        {
+            var transform = sphere.GetComponent<Transform>();
+
+            var y = transform.position.y;
+            outputs.Add(y);
+        }
+
+        return outputs.ToArray();
+    }
+
+    private void MoveAxis(System.IntPtr weights)
+    {
+        foreach (var white in whites)
+        {
+            var x = white.position.x;
+            var z = white.position.z;
+
+            double result = PanebWrapper.regression_point(weights, 2, new double[] { x, z });
+            Debug.Log("[" + x + ", " + z + "] = " + result);
+
+            white.position += Vector3.up * (float)result;
+        }
+    }
+
+    void Start()
+    {
+        var inputs = ComputeInputs();
+        var outputs = ComputeOutputs();
+        var weights = PanebWrapper.regression_compute(spheres.Length, 3, inputs, spheres.Length, 1, outputs);
+        MoveAxis(weights);
+    }
+}
