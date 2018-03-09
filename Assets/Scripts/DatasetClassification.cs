35c7b394a56c3db9f37837623a52f9b4692b482a
Mathieu Boisnard <m.boisnard@free.fr>
2018-03-09 11:39:48 +0100

    Start dataset analysis

---
+++ Assets/Scripts/DatasetClassification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatasetClassification : MonoBehaviour {
+
+    public TextAsset csv;
+
+	// Use this for initialization
+	void Start () {
+        
+        string[,] csvInputs = CSVReader.SplitCsvGrid(csv.text);
+
+        CSVReader.DebugOutputGrid(csvInputs);	
+	}
+}
